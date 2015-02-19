51cc85f8fdabcce9824bd42adb001fe8d3a73682
Daniel Abernathy <dabernathy89@gmail.com>
2015-02-19 07:21:40 -0600

    build out form

--- app/index.php
+++ app/index.php
@@ -1,4 +1,5 @@
 <ul>
     <li><a href="/step1/">Step 1</a></li>
     <li><a href="/step2/">Step 2</a></li>
+    <li><a href="/step3/">Step 3</a></li>
 </ul>
---
+++ app/step3/submit.php
@@ -0,0 +1,19 @@
+<?php
+    // $form_data = $_POST;
+    // echo "<pre>";
+    // print_r($form_data);
+    // echo "</pre>";
+
+    //  alternative:
+    //  var_dump($form_data);
+    //
+    //
+    //  echo "<br>";
+    //  echo $form_data['meme-top-text'];
+    //
+    //  echo "<br>";
+    //  echo $form_data['meme-bottom-text'];
+    //
+    //  echo "<br>";
+    //  echo $form_data['meme'];
+?>
