e517418eb50cb0986a81ae57daa3c15037bbfc24
Daniel Abernathy <dabernathy89@gmail.com>
2015-02-19 09:06:30 -0600

    make URLs relative

--- app/index.php
+++ app/index.php
@@ -1,9 +1,9 @@
 <ul>
-    <li><a href="/step1/">Step 1</a></li>
-    <li><a href="/step2/">Step 2</a></li>
-    <li><a href="/step3/">Step 3</a></li>
-    <li><a href="/step4/">Step 4</a></li>
-    <li><a href="/step5/">Step 5</a></li>
-    <li><a href="/step6/">Step 6</a></li>
-    <li><a href="/step7/">Step 7</a></li>
+    <li><a href="step1">Step 1</a></li>
+    <li><a href="step2">Step 2</a></li>
+    <li><a href="step3">Step 3</a></li>
+    <li><a href="step4">Step 4</a></li>
+    <li><a href="step5">Step 5</a></li>
+    <li><a href="step6">Step 6</a></li>
+    <li><a href="step7">Step 7</a></li>
 </ul>
